0a016c36b0c1f7ef87fc4e69b07a87c7f6c7af75
Sid <sid@gmail.com>
2018-07-25 23:28:44 +0530

    Initial Checkin

---
+++ Movies/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Movies.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name, int numTimes)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name} , numTimes is {numTimes}");
+        }
+    }
+}
